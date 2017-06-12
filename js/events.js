fae705f90eb936226f0f440946f1da4437acbc86
Maxen Haveles <mhaveles@gmail.com>
2017-06-12 20:48:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  alert('Hey!');
+}
+function frameIt() {
+  $('img').addClass('tasty');
+}
+function pressIt() {
+  $('form').on('keydown', function(key){
+    if (key.which == 71) {
+      alert('you pressed G');
+    }
+  })
+
+}
+function submitIt() {
+  alert('Your form is going to be submitted now');
+}
 
 $(document).ready(function(){
-
 // call functions here
+  $('p').on('click',getIt());  //hey alert
+  $('img').on('load', frameIt());  //red frame
+  //tests for keydown unsuccessful
+
+  //alert('worked')
+  pressIt();
+
+
+
+  $('form').on('submit', submitIt());     //submit button
 
 });
